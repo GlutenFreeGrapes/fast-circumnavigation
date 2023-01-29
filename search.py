08e15fb0be3bf72bb51ad6078c6d0587d2b69eba
GlutenFreeGrapes <daniellian238@gmail.com>
2023-01-29 15:42:38 -0500

    ran program for like 2 days
    ran the code for searching for a few days, managed to get some results

--- search.py
+++ search.py
@@ -6,7 +6,7 @@ dc={'IAD','DCA','BWI'}
 geod=Geodesic.WGS84
 aptdata=airportsdata.load('IATA')
 starttime=pytz.timezone('America/New_York').localize(datetime.datetime(2023,2,1,8,0,0))
-cutoff=pytz.timezone('America/New_York').localize(datetime.datetime(2023,2,3,15,0,0))
+cutoff=pytz.timezone('America/New_York').localize(datetime.datetime(2023,2,3,9,30,0))
 maxlayover=datetime.timedelta(hours=2)
 flights=pd.read_csv('better_all_flights_dropped.csv')
 longitudes={i:aptdata[i]['lon'] for i in set(flights['departure'])}
@@ -48,7 +48,7 @@ for i in airportdepartures:
 print('running')
 bing=time.perf_counter()
 print(bing-ding)
-fringe=[(0,0,0,100,0,datetime.timedelta(seconds=0).seconds,i,i,'',starttime) for i in dc]
+fringe=[(-500,0,0,0,100,datetime.timedelta(seconds=0).seconds,i,i,'',starttime) for i in dc]
 sols=[]
 def get_children(airport,dateandtime):
     airportflights=airporttoflights[airport]
@@ -60,21 +60,27 @@ def get_children(airport,dateandtime):
         if (nextavailflight['dep time']-dateandtime)<=maxlayover:
             children.add((nextavailflight['arr time'],nextavailflight['distance'],nextavailflight['arrival'],nextavailflight['flight number']))
     return children
-
-
-while len(fringe)>0:
-    (dcdist,curdist,ew,efficiency,numflights,timediff,airport,aptpath,flightstaken,curtime)=heapq.heappop(fringe)
+with open('possible solutions.txt','w')as f:
+    f.write('LIST OF SOLUTIONS\n-----------------')
+pcount=0
+solct=0
+while len(fringe)>0 and solct<5000:
+    (ew,dcdist,numflights,curdist,efficiency,timediff,airport,aptpath,flightstaken,curtime)=heapq.heappop(fringe)
     # print((numflights,'east' if ew==-1 else 'west',-efficiency,timediff,curdist,airport,aptpath,flightstaken,curtime))
     # input()
-    if airport in dc and (-curdist)>36500:
-        sols.append((curtime,curdist,aptpath,flightstaken))
-        with open('possible solutions.txt','w') as f:
-            f.write('\n'.join([str(i) for i in sols]))
+    pcount+=1
+    if pcount%500==0:
+        print(-ew,dcdist,numflights,curdist,efficiency,timediff,airport,aptpath,flightstaken,curtime)
+    if airport in dc and (-curdist)>36900:
+        # sols.append((curtime,curdist,aptpath,flightstaken))
+        solct+=1
+        with open('possible solutions.txt','a') as f:
+            f.write('\n'+', '.join([str(i) for i in (curtime,curdist,aptpath,flightstaken,timediff,efficiency)]))
     else:
         for newtime,dist,newairport,flightnum in get_children(airport,curtime):
-            if numflights<8:
+            if numflights<10:
                 ntdiff=newtime-starttime
                 ndist=curdist-dist
-                heapq.heappush(fringe,(londisttodc[newairport],ndist,-eastwestbound[(airport,newairport)],-ntdiff.seconds/ndist,numflights+1,ntdiff,newairport,aptpath+' - '+newairport,flightstaken+' - '+flightnum,newtime))
+                heapq.heappush(fringe,(-eastwestbound[(airport,newairport)],londisttodc[newairport],numflights+1,ndist,-ntdiff.seconds/ndist,ntdiff,newairport,aptpath+' - '+newairport,flightstaken+' - '+flightnum,newtime))
 print(time.perf_counter()-bing)
     
